5380da6bb1b8c867703213fc06fbae6f505b6db6
korompaidani <korompaidani@gmail.com>
2018-11-01 20:24:43 +0100

    First commit copy to repo

---
+++ GoogleMap/GoogleMap/CoordinatePickerForm.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace GoogleMap
+{
+    partial class CoordinatePickerForm
+    {        
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.WebBrowser myBrowser;
+        private System.Windows.Forms.TextBox txtStreet;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZip;
+        private System.Windows.Forms.Label lblStreet;
+        private System.Windows.Forms.Label lblCity; 
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblZip;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblCoord;
+
+        private System.Drawing.Rectangle axisRectX;
+        private System.Drawing.Graphics lineX;
+        
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CoordinatePickerForm));            
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.myBrowser = new System.Windows.Forms.WebBrowser();
+            this.txtStreet = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZip = new System.Windows.Forms.TextBox();
+            this.lblStreet = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblZip = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblCoord = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(400, 755);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // myBrowser
+            // 
+            this.myBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.myBrowser.Location = new System.Drawing.Point(0, 0);
+            this.myBrowser.Margin = new System.Windows.Forms.Padding(450, 5, 4, 5);
+            this.myBrowser.MinimumSize = new System.Drawing.Size(30, 31);
+            this.myBrowser.Name = "myBrowser";
+            this.myBrowser.Size = new System.Drawing.Size(1202, 755);
+            this.myBrowser.TabIndex = 2;            
+            this.myBrowser.Url = new System.Uri("http://maps.google.com", System.UriKind.Absolute);
+            this.myBrowser.Navigated += new System.Windows.Forms.WebBrowserNavigatedEventHandler(this.MyBrowser_Navigated);
+            // 
+            // txtStreet
+            // 
+            this.txtStreet.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStreet.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtStreet.Location = new System.Drawing.Point(111, 43);
+            this.txtStreet.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtStreet.Name = "txtStreet";
+            this.txtStreet.Size = new System.Drawing.Size(265, 26);
+            this.txtStreet.TabIndex = 2;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtCity.Location = new System.Drawing.Point(111, 121);
+            this.txtCity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(265, 26);
+            this.txtCity.TabIndex = 3;
+            this.txtCity.Text = "Zalaegerszeg";
+            // 
+            // txtState
+            // 
+            this.txtState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtState.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtState.Location = new System.Drawing.Point(111, 199);
+            this.txtState.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(265, 26);
+            this.txtState.TabIndex = 4;
+            // 
+            // txtZip
+            // 
+            this.txtZip.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtZip.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtZip.Location = new System.Drawing.Point(111, 277);
+            this.txtZip.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtZip.Name = "txtZip";
+            this.txtZip.Size = new System.Drawing.Size(265, 26);
+            this.txtZip.TabIndex = 5;
+            // 
+            // lblStreet
+            // 
+            this.lblStreet.AutoSize = true;
+            this.lblStreet.Location = new System.Drawing.Point(37, 42);
+            this.lblStreet.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStreet.Name = "lblStreet";
+            this.lblStreet.Size = new System.Drawing.Size(64, 25);
+            this.lblStreet.TabIndex = 6;
+            this.lblStreet.Text = "Street";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(37, 113);
+            this.lblCity.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(46, 25);
+            this.lblCity.TabIndex = 7;
+            this.lblCity.Text = "City";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(37, 198);
+            this.lblState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(58, 25);
+            this.lblState.TabIndex = 8;
+            this.lblState.Text = "State";
+            // 
+            // lblZip
+            // 
+            this.lblZip.AutoSize = true;
+            this.lblZip.Location = new System.Drawing.Point(37, 276);
+            this.lblZip.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblZip.Name = "lblZip";
+            this.lblZip.Size = new System.Drawing.Size(39, 25);
+            this.lblZip.TabIndex = 9;
+            this.lblZip.Text = "Zip";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnSearch.Location = new System.Drawing.Point(42, 350);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(334, 58);
+            this.btnSearch.TabIndex = 10;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblCoord
+            // 
+            this.lblCoord.AutoSize = true;
+            this.lblCoord.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCoord.Location = new System.Drawing.Point(37, 429);
+            this.lblCoord.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCoord.Name = "lblCoord";
+            this.lblCoord.Size = new System.Drawing.Size(118, 25);
+            this.lblCoord.TabIndex = 11;
+            this.lblCoord.Text = "Coordinates";
+            // 
+            // CoordinatePickerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(1202, 755);
+            this.Controls.Add(this.lblCoord);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblZip);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblStreet);
+            this.Controls.Add(this.txtZip);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtStreet);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.myBrowser);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "CoordinatePickerForm";
+            this.Text = "GPS Coordinate Picker Tool by Daniel Korompai";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CoordinatePickerForm_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
---
+++ GoogleMap/GoogleMap/CoordinatePickerForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GoogleMap
+{   
+    public partial class CoordinatePickerForm : Form
+    {      
+        public CoordinatePickerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string street = txtStreet.Text;
+            string city = txtCity.Text;
+            string state = txtState.Text;
+            string zip = txtZip.Text;
+
+            try
+            {
+                StringBuilder queryAddres = new StringBuilder();
+                queryAddres.Append("http://maps.google.com/maps?q=");
+
+                if(street != string.Empty)
+                {
+                    queryAddres.Append(street + "," + "+");
+                }
+                if (city != string.Empty)
+                {
+                    queryAddres.Append(city + "," + "+");
+                }
+                if (state != string.Empty)
+                {
+                    queryAddres.Append(state + "," + "+");
+                }
+                if (zip != string.Empty)
+                {
+                    queryAddres.Append(zip + "," + "+");
+                }
+
+                myBrowser.Navigate(queryAddres.ToString());
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show(exc.Message.ToString(), "Error");
+            }
+        }
+
+        private void MyBrowser_Navigated(object sender, WebBrowserNavigatedEventArgs e)
+        {
+            string entireUrl = myBrowser.Url.ToString();
+            string firstCoordinate = null;
+            string secondCoordinate = null;
+
+            if(entireUrl != string.Empty)
+            {
+                if (entireUrl.Contains("@"))
+                {
+                    IList<string> listOfString = new List<string>();
+
+                    listOfString = entireUrl.Split('@');
+
+                    var list = listOfString[1].Split(',');
+
+                    firstCoordinate = list[0];
+                    secondCoordinate = list[1];
+                }
+            }
+
+            StringBuilder coordinates = new StringBuilder();
+            coordinates.Append(firstCoordinate + ", " + secondCoordinate);
+            lblCoord.Text = coordinates.ToString();
+        }
+
+        private void CoordinatePickerForm_Paint(object sender, PaintEventArgs e)
+        {                        
+            Graphics verticalLine = e.Graphics;
+            Pen blackPen = new Pen(Color.Black, 1);
+            verticalLine.DrawLine(blackPen, 50, 50, 100, 100);
+            verticalLine.Dispose();
+        }
+    }
+}
